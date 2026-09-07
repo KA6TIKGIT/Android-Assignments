b9f67cafcb6daddb1c58dfcee648b3656e47b9bc
KA6TIKGIT <kartikmeti2006@gmail.com>
2026-09-07 18:49:20 +0530

    Update Assignment3.kt

--- Assignment3.kt
+++ Assignment3.kt
@@ -1,14 +1,20 @@
 package com.pes.lib
 
-// functional programming, collections
 fun main() {
 
-    val phoneNumbers = listOf<String>("1234567890", "123", "", "0987654321")
+    val phoneNumbers = listOf<String>(
+        "1234567890", "123", "", "0987654321"
+    )
 
-    // 1. using lambda, filter valid phone numbers - 10 digits
-    // filter()
+    // 1. Filter valid phone numbers
+    val validNumbers = phoneNumbers.filter {
+        it.length == 10
+    }
 
+    println(validNumbers)
 
-    // 2. using lambda, print all phone numbers, if invalid - print 'NAN'
-    // forEach(), map()
+    // 2. Print NAN for invalid numbers
+    phoneNumbers
+        .map { if (it.length == 10) it else "NAN" }
+        .forEach { println(it) }
 }
